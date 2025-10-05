ae0bba994a0c25de78ad2463062fef8347ff587c
werllyson <werllyson@outlook.com>
2025-10-05 12:22:23 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+# Lista de filmes por categoria
+filmes_por_categoria = {
+    'Ação': ['Vingadores: Ultimato', 'Mad Max: Estrada da Fúria', 'John Wick'],
+    'Comédia': ['Corra Que A Policia Vem Ai', 'Se Beber, Não Case', 'Superbad - É Hoje'],
+    'Drama': ['A Vida É Bela', 'O Resgate Do Soldado Rayan', 'A Lista de Schindler'],
+    'Terror': ['Invocação do Mal', 'O Exorcista', 'Sobrenatural'],
+    'Aventura': ['Indiana Jones', 'Jurassic Park', 'Piratas do Caribe'],
+    'Ficção Científica': ['Interestelar', 'Efeito Borboleta', 'A Chegada']
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    filme_sugerido = None
+    if request.method == 'POST':
+        categoria = request.form['categoria']
+        if categoria in filmes_por_categoria:
+            filme_sugerido = random.choice(filmes_por_categoria[categoria])
+    return render_template('index.html', filme=filme_sugerido)
+
+if __name__ == '__main__':
+    app.run(debug=True)
